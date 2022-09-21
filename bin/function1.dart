9a6fcf1358707149311bfc136f95324b0fd6daab
arshad <arshadattupurath23@gmail.com>
2022-09-21 14:18:19 +0530

    Initial commit

---
+++ bin/function1.dart
@@ -0,0 +1,5 @@
+import 'package:function1/function1.dart' as function1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${function1.calculate()}!');
+}
---
+++ test/function1_test.dart
@@ -0,0 +1,8 @@
+import 'package:function1/function1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
